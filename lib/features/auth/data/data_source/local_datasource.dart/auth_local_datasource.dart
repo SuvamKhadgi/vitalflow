9a99cb1e89cf9aaa73505706627fb2ac793783c5
SuvamKhadgi <appleb986@gmail.com>
2025-01-27 11:56:30 +0545

    auth local datasoucre done

---
+++ lib/features/auth/data/data_source/local_datasource.dart/auth_local_datasource.dart
@@ -0,0 +1,44 @@
+import 'package:vitalflow/core/network/hive_service.dart';
+import 'package:vitalflow/features/auth/data/data_source/auth_data_source.dart';
+import 'package:vitalflow/features/auth/data/model/auth_hive_model.dart';
+import 'package:vitalflow/features/auth/domain/entity/auth_entity.dart';
+
+class AuthLocalDataSource implements IAuthDataSource {
+  final HiveService _hiveService;
+
+  AuthLocalDataSource(this._hiveService);
+
+  @override
+  Future<AuthEntity> getCurrentUser() {
+    // Return Empty AuthEntity
+    return Future.value(const AuthEntity(
+      userId: "1",
+      name: "",
+      email: "",
+      password: "",
+    ));
+  }
+
+  @override
+  Future<String> loginUser(String username, String password) async {
+    try {
+      await _hiveService.login(username, password);
+      return Future.value("Success");
+    } catch (e) {
+      return Future.error(e);
+    }
+  }
+
+  @override
+  Future<void> signupUser(AuthEntity user) async {
+    try {
+      // Convert AuthEntity to AuthHiveModel
+      final authHiveModel = AuthHiveModel.fromEnity(user);
+
+      await _hiveService.register(authHiveModel);
+      return Future.value();
+    } catch (e) {
+      return Future.error(e);
+    }
+  }
+}
