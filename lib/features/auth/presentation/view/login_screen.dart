a536e68cb8c98398e8cda616aa07d10226008e71
SuvamKhadgi <appleb986@gmail.com>
2025-01-27 11:59:25 +0545

    made login screen

---
+++ lib/features/auth/presentation/view/login_screen.dart
@@ -0,0 +1,187 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:vitalflow/features/auth/presentation/view/sign_up.dart';
+import 'package:vitalflow/features/auth/presentation/view_model/login/login_bloc.dart';
+
+class LoginScreen extends StatelessWidget {
+  LoginScreen({super.key});
+
+  // void _showSuccessSnackbar(BuildContext context) {
+  //   ScaffoldMessenger.of(context).showSnackBar(
+  //     const SnackBar(
+  //       content: Text('Login successfully!'),
+  //       backgroundColor: Colors.green,
+  //       duration: Duration(seconds: 2),
+  //     ),
+  //   );
+  // }
+
+  final _formKey = GlobalKey<FormState>();
+  // final _emailController = TextEditingController(text: 'suvam');
+  final _emailController = TextEditingController();
+  // final _passwordController = TextEditingController(text: 'suvam123');
+  final _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    // final TextEditingController emailController = TextEditingController();
+    // final TextEditingController passwordController = TextEditingController();
+
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color.fromARGB(255, 149, 202, 246),
+              Colors.lightBlueAccent
+            ],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: Form(
+          key: _formKey,
+          child: Center(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(24.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    // Logo
+                    Center(
+                      child: Column(
+                        children: [
+                          Image.asset(
+                            'assets/images/logos.png',
+                            width: 150,
+                            height: 150,
+                            fit: BoxFit.cover,
+                          ),
+                          const SizedBox(height: 10),
+                          const Text(
+                            "Welcome Back!",
+                            style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
+                          const SizedBox(height: 5),
+                          const Text(
+                            "Login to continue",
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.white70,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 30),
+
+                    // Email Field
+                    TextField(
+                      controller: _emailController,
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white,
+                        labelText: 'Email',
+                        hintText: 'Enter your email',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide: BorderSide.none,
+                        ),
+                        prefixIcon: const Icon(Icons.email, color: Colors.blue),
+                      ),
+                      keyboardType: TextInputType.emailAddress,
+                    ),
+                    const SizedBox(height: 16),
+
+                    // Password Field
+                    TextField(
+                      controller: _passwordController,
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white,
+                        labelText: 'Password',
+                        hintText: 'Enter your password',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide: BorderSide.none,
+                        ),
+                        prefixIcon: const Icon(Icons.lock, color: Colors.blue),
+                      ),
+                      obscureText: true,
+                    ),
+                    const SizedBox(height: 20),
+
+                    // Login Button
+                    ElevatedButton(
+                      onPressed: () async {
+                        if (_formKey.currentState!.validate()) {
+                          context.read<LoginBloc>().add(
+                                LoginUserEvent(
+                                  context: context,
+                                  email: _emailController.text,
+                                  password: _passwordController.text,
+                                ),
+                              );
+                          // _showSuccessSnackbar(context);
+                        }
+                      },
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                        padding: const EdgeInsets.symmetric(vertical: 16),
+                        backgroundColor: Colors.white,
+                      ),
+                      child: const Text(
+                        'Login',
+                        style: TextStyle(
+                          color: Colors.blue,
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+
+                    // Sign Up Option
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text(
+                          "Don't have an account? ",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            context.read<LoginBloc>().add(
+                                  NavigateSignupScreenEvent(
+                                    destination: const SignUp(),
+                                    context: context,
+                                  ),
+                                );
+                          },
+                          child: const Text(
+                            'Sign Up',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
