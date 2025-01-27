172c667628fbfc3071c007993f386d5366c386e9
SuvamKhadgi <appleb986@gmail.com>
2025-01-27 11:53:47 +0545

    main updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:vitalflow/app/di/di.dart';
 import 'package:vitalflow/app/my_app.dart';
+import 'package:vitalflow/core/network/hive_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // await HiveService().init();
+  await HiveService.init();
   await initDependencies();
   runApp(const MyApp());
 }
