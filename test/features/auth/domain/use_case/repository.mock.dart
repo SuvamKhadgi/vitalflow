e061c2f672bf92d85b88257c45b79ba5ad1fa1f2
SuvamKhadgi <appleb986@gmail.com>
2025-02-07 16:15:20 +0545

    made mock repository

---
+++ test/features/auth/domain/use_case/repository.mock.dart
@@ -0,0 +1,7 @@
+import 'package:mocktail/mocktail.dart';
+import 'package:vitalflow/features/auth/domain/repository/auth_repoitory.dart';
+
+class MockAuthRepository extends Mock implements IAuthRepoitory
+{
+
+}
