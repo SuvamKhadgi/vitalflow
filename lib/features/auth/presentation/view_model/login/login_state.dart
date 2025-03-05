f237ab64537becb55ee7b5adcab0316a5dd7f1e4
SuvamKhadgi <appleb986@gmail.com>
2025-03-06 02:03:59 +0545

    modified login state

--- lib/features/auth/presentation/view_model/login/login_state.dart
+++ lib/features/auth/presentation/view_model/login/login_state.dart
@@ -1,11 +1,43 @@
+// part of 'login_bloc.dart';
+
+// class LoginState {
+//   // const LoginState();
+//   final bool isLoading;
+//   final bool isSuccess;
+
+//   LoginState({
+//     required this.isLoading,
+//     required this.isSuccess,
+//   });
+
+//   LoginState.initial()
+//       : isLoading = false,
+//         isSuccess = false;
+
+//   LoginState copyWith({
+//     bool? isLoading,
+//     bool? isSuccess,
+//   }) {
+//     return LoginState(
+//       isLoading: isLoading ?? this.isLoading,
+//       isSuccess: isSuccess ?? this.isSuccess,
+//     );
+//   }
+
+//   // @override
+//   // List<Object> get props => [];
+// }
+
+// // final class LoginInitial extends LoginState {}
+
+
 part of 'login_bloc.dart';
 
-class LoginState {
-  // const LoginState();
+class LoginState extends Equatable {
   final bool isLoading;
   final bool isSuccess;
 
-  LoginState({
+  const LoginState({
     required this.isLoading,
     required this.isSuccess,
   });
@@ -24,8 +56,6 @@ class LoginState {
     );
   }
 
-  // @override
-  // List<Object> get props => [];
+  @override
+  List<Object?> get props => [isLoading, isSuccess];
 }
-
-// final class LoginInitial extends LoginState {}
