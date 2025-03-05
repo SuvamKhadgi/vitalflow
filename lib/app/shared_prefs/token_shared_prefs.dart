03067f93c6c396dc62ef067df453bddd8f8c9988
SuvamKhadgi <appleb986@gmail.com>
2025-03-06 01:44:25 +0545

    updated shared preferances to remove token

--- lib/app/shared_prefs/token_shared_prefs.dart
+++ lib/app/shared_prefs/token_shared_prefs.dart
@@ -10,7 +10,8 @@ class TokenSharedPrefs {
   Future<Either<Failure, void>> saveToken(String token) async {
     try {
       await _sharedPreferences.setString('token', token);
-      return  const Right(null);
+      print('TokenSharedPrefs: Token saved: $token');
+      return const Right(null);
     } catch (e) {
       return Left(SharedPrefsFailure(message: e.toString()));
     }
@@ -19,7 +20,18 @@ class TokenSharedPrefs {
   Future<Either<Failure, String>> getToken() async {
     try {
       final token = _sharedPreferences.getString('token');
+      print('TokenSharedPrefs: Retrieved token: "$token"');
       return Right(token ?? '');
+    } catch (e) {
+      return Left(SharedPrefsFailure(message: e.toString()));
+    }
+  }
+
+  Future<Either<Failure, void>> clearToken() async {
+    try {
+      await _sharedPreferences.remove('token');
+      print('TokenSharedPrefs: Token cleared');
+      return const Right(null);
     } catch (e) {
       return Left(SharedPrefsFailure(message: e.toString()));
     }
