7dab4aba4c6d7f89f3bc43d7b4903b25b086004d
SuvamKhadgi <appleb986@gmail.com>
2025-03-06 02:05:02 +0545

    made new fuction on signup bloc

--- lib/features/auth/presentation/view_model/signup/signup_bloc.dart
+++ lib/features/auth/presentation/view_model/signup/signup_bloc.dart
@@ -1,11 +1,96 @@
-import 'dart:io';
+// import 'dart:io';
+// import 'package:equatable/equatable.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_bloc/flutter_bloc.dart';
+// import 'package:vitalflow/app/di/di.dart';
+// import 'package:vitalflow/core/common/snackbar/my_snackbar.dart';
+// import 'package:vitalflow/features/auth/domain/use_case/signup_usecase.dart';
+// import 'package:vitalflow/features/auth/domain/use_case/upload_image_usecase.dart';
+// import 'package:vitalflow/features/auth/presentation/view/login_screen.dart';
+// import 'package:vitalflow/features/auth/presentation/view_model/login/login_bloc.dart';
+// import 'package:vitalflow/features/home/presentation/view_model/home_bloc.dart';
 
+// part 'signup_event.dart';
+// part 'signup_state.dart';
+
+// class SignUpBloc extends Bloc<SignupEvent, SignupState> {
+//   final SignupUsecase _signupUsecase;
+//   final UploadImageUsecase _uploadImageUsecase;
+
+//   SignUpBloc({
+//     required SignupUsecase signupUseCase,
+//     required UploadImageUsecase uploadImageUsecase,
+//   })  : _signupUsecase = signupUseCase,
+//         _uploadImageUsecase = uploadImageUsecase,
+//         super(const SignupState.initial()) {
+//     on<SignupUser>(_onSignupEvent);
+//     on<LoadImage>(_onLoadImage);
+//     on<NavigateloginScreenEvent>(_onNavigateLoginScreen);
+//   }
+
+//   void _onSignupEvent(SignupUser event, Emitter<SignupState> emit) async {
+//     debugPrint("📢 Signing up with image: ${state.imageName}");
+//     emit(state.copyWith(isLoading: true));
+//     final result = await _signupUsecase.call(SignupUserParams(
+//       name: event.name,
+//       image: state.imageName,
+//       email: event.email,
+//       password: event.password,
+//     ));
+
+//     result.fold(
+//       (l) {
+//         emit(state.copyWith(isLoading: false, isSuccess: false));
+//         showMySnackBar(context: event.context, message: "Signup Failed: ${l.message}", color: Colors.red);
+//       },
+//       (r) {
+//         emit(state.copyWith(isLoading: false, isSuccess: true));
+//         showMySnackBar(context: event.context, message: "Registration Successful", color: Colors.green);
+//         add(NavigateloginScreenEvent(context: event.context, destination: LoginScreen()));
+//       },
+//     );
+//   }
+
+//   void _onLoadImage(LoadImage event, Emitter<SignupState> emit) async {
+//     emit(state.copyWith(isLoading: true));
+//     final result = await _uploadImageUsecase.call(UploadImageParams(file: event.file));
+//     result.fold(
+//       (l) => emit(state.copyWith(isLoading: false, isSuccess: false)),
+//       (r) {
+//         emit(state.copyWith(isLoading: false, isSuccess: true, imageName: r));
+//       },
+//     );
+//   }
+
+//   void _onNavigateLoginScreen(NavigateloginScreenEvent event, Emitter<SignupState> emit) {
+//     Navigator.push(
+//       event.context,
+//       MaterialPageRoute(
+//         builder: (context) => MultiBlocProvider(
+//           providers: [
+//             BlocProvider.value(value: getIt<LoginBloc>()),
+//             BlocProvider.value(value: getIt<SignUpBloc>()),
+//             BlocProvider.value(value: getIt<HomeBloc>()),
+//           ],
+//           child: event.destination,
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+
+import 'dart:io';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:vitalflow/app/di/di.dart';
 import 'package:vitalflow/core/common/snackbar/my_snackbar.dart';
 import 'package:vitalflow/features/auth/domain/use_case/signup_usecase.dart';
 import 'package:vitalflow/features/auth/domain/use_case/upload_image_usecase.dart';
+import 'package:vitalflow/features/auth/presentation/view/login_screen.dart';
+import 'package:vitalflow/features/auth/presentation/view_model/login/login_bloc.dart';
+import 'package:vitalflow/features/home/presentation/view_model/home_bloc.dart';
 
 part 'signup_event.dart';
 part 'signup_state.dart';
@@ -17,23 +102,16 @@ class SignUpBloc extends Bloc<SignupEvent, SignupState> {
   SignUpBloc({
     required SignupUsecase signupUseCase,
     required UploadImageUsecase uploadImageUsecase,
-    // required LoginUseCase loginUseCase
-    // required LoginBloc loginBloc
   })  : _signupUsecase = signupUseCase,
         _uploadImageUsecase = uploadImageUsecase,
-        // _loginUseCase = loginUseCase,
-        // _loginBloc = loginBloc,
         super(const SignupState.initial()) {
     on<SignupUser>(_onSignupEvent);
     on<LoadImage>(_onLoadImage);
+    on<NavigateloginScreenEvent>(_onNavigateLoginScreen);
   }
 
-  void _onSignupEvent(
-    SignupUser event,
-    Emitter<SignupState> emit,
-  ) async {
+  void _onSignupEvent(SignupUser event, Emitter<SignupState> emit) async {
     debugPrint("📢 Signing up with image: ${state.imageName}");
-
     emit(state.copyWith(isLoading: true));
     final result = await _signupUsecase.call(SignupUserParams(
       name: event.name,
@@ -43,27 +121,42 @@ class SignUpBloc extends Bloc<SignupEvent, SignupState> {
     ));
 
     result.fold(
-      (l) => emit(state.copyWith(isLoading: false, isSuccess: false)),
+      (l) {
+        emit(state.copyWith(isLoading: false, isSuccess: false));
+        showMySnackBar(context: event.context, message: l.message, color: Colors.red);
+      },
       (r) {
         emit(state.copyWith(isLoading: false, isSuccess: true));
-        showMySnackBar(
-            context: event.context, message: "Registration Successful");
+        showMySnackBar(context: event.context, message: "Registration Successful", color: Colors.green);
+        add(NavigateloginScreenEvent(context: event.context, destination: LoginScreen()));
       },
     );
   }
 
-  void _onLoadImage(
-    LoadImage event,
-    Emitter<SignupState> emit,
-  ) async {
+  void _onLoadImage(LoadImage event, Emitter<SignupState> emit) async {
     emit(state.copyWith(isLoading: true));
-    final result =
-        await _uploadImageUsecase.call(UploadImageParams(file: event.file));
+    final result = await _uploadImageUsecase.call(UploadImageParams(file: event.file));
     result.fold(
       (l) => emit(state.copyWith(isLoading: false, isSuccess: false)),
       (r) {
         emit(state.copyWith(isLoading: false, isSuccess: true, imageName: r));
       },
+    );
+  }
+
+  void _onNavigateLoginScreen(NavigateloginScreenEvent event, Emitter<SignupState> emit) {
+    Navigator.push(
+      event.context,
+      MaterialPageRoute(
+        builder: (context) => MultiBlocProvider(
+          providers: [
+            BlocProvider.value(value: getIt<LoginBloc>()),
+            BlocProvider.value(value: getIt<SignUpBloc>()),
+            BlocProvider.value(value: getIt<HomeBloc>()),
+          ],
+          child: event.destination,
+        ),
+      ),
     );
   }
 }
